8919dfc7d1866bb518ba1456d00f6f5788d821a8
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-25 18:04:25 +0200

    Remove default project Cinema-NLayer-MVC
    Removed the default project "Cinema-NLayer-MVC" that was automatically created when initializing the solution in Visual Studio.
    
    Since this project was not needed for the architecture

--- Cinema-Nlayer-MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cinema_Nlayer_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
