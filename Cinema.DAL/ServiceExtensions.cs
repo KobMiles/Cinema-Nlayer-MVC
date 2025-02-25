e8e57fd5f840ec022cbccd927ab7bd0fd7fa98d8
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-25 19:48:48 +0200

    Add DbContext registration in ServiceExtensions
    Introduce using directives and a new method to register
    CinemaDbContext with a SQL Server connection string in
    the dependency injection container.

--- Cinema.DAL/ServiceExtensions.cs
+++ Cinema.DAL/ServiceExtensions.cs
@@ -1,5 +1,14 @@
-﻿namespace Cinema.DAL;
+﻿using Cinema.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cinema.DAL;
 
 public static class ServiceExtensions
 {
+    public static void AddDbContext(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<CinemaDbContext>(x =>
+            x.UseSqlServer(connectionString));
+    }
 }
