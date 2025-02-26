3dff5b4c5c638b92af61e9a01b1bf4737a409e1f
Nikita Kobylynskyi <148866440+KobMiles@users.noreply.github.com>
2025-02-26 11:30:42 +0200

    Feature/1 add dbcontext unit tests (#2)
    * Make CinemaDbContext public and enhance constructor
    
    Changed CinemaDbContext from internal to public for external access. Added null check for options parameter in the constructor to improve robustness.
    
    * Add unit tests for CinemaDbContext
    
    Introduced the `CinemaDbContextTests` class in the `Cinema.Tests.DAL.Data` namespace. This class contains two tests: one to ensure that a `CinemaDbContext` can be instantiated with valid options and is not null, and another to verify that an `ArgumentNullException` is thrown when null options are provided.
    
    * Update Cinema.Tests.csproj for .NET 8.0 and dependencies
    
    Refactor project file to use .NET SDK format targeting .NET 8.0.
    Enable implicit usings and nullable reference types.
    Add package references for testing and Entity Framework.
    Define folder structure and include project reference to Cinema.DAL.
    
    * Add Cinema.Tests project to solution
    
    This commit introduces a new project named "Cinema.Tests" to the `Cinema-Nlayer-MVC.sln` solution. The project path is `Cinema.Tests\Cinema.Tests.csproj`, and it includes a unique project GUID `{2240EB8A-3100-47A9-A8B5-9D7B7DD1C580}`. Additionally, configurations for both Debug and Release modes have been added to the solution's global section.

---
+++ Cinema.Tests/DAL/Data/CinemaDbContextTests.cs
@@ -0,0 +1,24 @@
+﻿using Cinema.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Tests.DAL.Data;
+public class CinemaDbContextTests
+{
+    [Fact]
+    public void CanCreateCinemaDbContext_WhenOptionsProvided_ShouldNotBeNull()
+    {
+        var options = new DbContextOptionsBuilder<CinemaDbContext>()
+            .UseInMemoryDatabase("TestDb")
+            .Options;
+
+        using var dbContext = new CinemaDbContext(options);
+
+        Assert.NotNull(dbContext);
+    }
+
+    [Fact]
+    public void CanCreateCinemaDbContext_WhenOptionsNotProvided_ShouldThrowArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new CinemaDbContext(null!));
+    }
+}
