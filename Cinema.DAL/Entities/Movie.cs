4fc1ea31da4d57bcbc0d446e92da6a4d32a5227f
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-27 15:30:25 +0200

    Add Movie class to Cinema.DAL.Entities namespace
    Introduces the `Movie` class implementing the `IEntity` interface.
    Includes properties for `Id`, `Name`, `Description`, `RatingScore`,
    `Duration`, `ReleaseDate`, `PosterUrl`, `TrailerUrl`, and a collection
    of `Genres`. Non-nullable properties are initialized, while some
    properties are nullable to allow for optional values.

---
+++ Cinema.DAL/Entities/Movie.cs
@@ -0,0 +1,15 @@
+﻿using Cinema.DAL.Interfaces;
+
+namespace Cinema.DAL.Entities;
+public class Movie : IEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public double RatingScore { get; set; }
+    public TimeSpan Duration { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public string PosterUrl { get; set; } = null!;
+    public string TrailerUrl { get; set; } = null!;
+    public ICollection<Genre> Genres { get; set; } = new List<Genre>();
+}
