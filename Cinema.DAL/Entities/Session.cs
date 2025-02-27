458c9c33b004d66f50b5b9d360300e4bf64f1fa5
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-27 17:03:43 +0200

    Add Session class to manage movie session details
    Introduces a new `Session` class in the `Cinema.DAL.Entities` namespace. This class implements the `IEntity` interface and defines properties for session management, including `Id`, `MovieId`, `Movie`, `HallId`, `Hall`, `StartTime`, and `TicketPrice`. The `Movie` and `Hall` properties are set to `null!` to indicate they will be assigned later.

---
+++ Cinema.DAL/Entities/Session.cs
@@ -0,0 +1,17 @@
+﻿using Cinema.DAL.Interfaces;
+
+namespace Cinema.DAL.Entities;
+
+public class Session : IEntity
+{
+    public int Id { get; set; }
+
+    public int MovieId { get; set; }
+    public Movie Movie { get; set; } = null!;
+
+    public int HallId { get; set; }
+    public Hall Hall { get; set; } = null!;
+
+    public DateTime StartTime { get; set; }
+    public decimal TicketPrice { get; set; }
+}
