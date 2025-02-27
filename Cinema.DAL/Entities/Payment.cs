54d6ae0343b3f553494f1b16fbac41eac8dbb69b
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-28 01:07:08 +0200

    Add Payment class to manage transaction details
    Introduced a new `Payment` class in the `Cinema.DAL.Entities` namespace. This class implements the `IEntity` interface and includes properties for `Id`, `Amount`, `Date`, and `PaymentMethod`. It also features a collection of `Ticket` objects, initialized as an empty collection.

---
+++ Cinema.DAL/Entities/Payment.cs
@@ -0,0 +1,12 @@
+﻿using Cinema.DAL.Interfaces;
+
+namespace Cinema.DAL.Entities;
+public class Payment : IEntity
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string PaymentMethod { get; set; } = null!;
+
+    public ICollection<Ticket> Tickets { get; set; } = [];
+}
