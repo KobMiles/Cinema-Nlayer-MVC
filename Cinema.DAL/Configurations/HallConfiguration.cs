e785a4e96dc51fab8819fd8c2ad032875d24a566
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-27 17:03:51 +0200

    Add HallConfiguration for entity setup
    Introduced the `HallConfiguration` class in the `Cinema.DAL.Configurations` namespace. This class configures the `Hall` entity using Entity Framework Core's `EntityTypeBuilder`, defining the primary key and establishing a one-to-many relationship with the `Session` entity, utilizing `HallId` as the foreign key.

---
+++ Cinema.DAL/Configurations/HallConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Cinema.DAL.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cinema.DAL.Configurations;
+
+public class HallConfiguration
+{
+    public void Configure(EntityTypeBuilder<Hall> builder)
+    {
+        builder
+            .HasKey(h => h.Id);
+
+        builder
+            .HasMany(h => h.Sessions)
+            .WithOne(s => s.Hall)
+            .HasForeignKey(s => s.HallId);
+    }
+}
