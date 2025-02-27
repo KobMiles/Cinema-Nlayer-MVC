a73d8dacf8439d4dd71e46fbc524318d4c74ed9a
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-27 17:03:59 +0200

    Add SessionConfiguration for entity setup
    Introduces a new `SessionConfiguration` class in the
    `Cinema.DAL.Configurations` namespace. This class configures
    the `Session` entity with required properties and establishes
    relationships with `Movie` and `Hall` entities, ensuring
    referential integrity through foreign keys.

---
+++ Cinema.DAL/Configurations/SessionConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Cinema.DAL.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cinema.DAL.Configurations;
+public class SessionConfiguration
+{
+    public void Configure(EntityTypeBuilder<Session> builder)
+    {
+        builder
+            .HasKey(s => s.Id);
+
+        builder
+            .Property(s => s.MovieId)
+            .IsRequired();
+
+        builder
+            .Property(s => s.HallId)
+            .IsRequired();
+
+        builder
+            .Property(s => s.StartTime)
+            .IsRequired();
+
+        builder
+            .Property(s => s.TicketPrice)
+            .IsRequired();
+
+        builder
+            .HasOne(s => s.Movie)
+            .WithMany(m => m.Sessions)
+            .HasForeignKey(s => s.MovieId);
+
+        builder
+            .HasOne(s => s.Hall)
+            .WithMany(h => h.Sessions)
+            .HasForeignKey(s => s.HallId);
+    }
+}
