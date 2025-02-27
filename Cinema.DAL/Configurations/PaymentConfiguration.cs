a7bb5f93300fd720334b442c02d2a6a276af337a
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-28 01:08:14 +0200

    Add PaymentConfiguration for Entity Framework setup
    Introduces the `PaymentConfiguration` class in the
    `Cinema.DAL.Configurations` namespace to configure the
    `Payment` entity. This includes setting the primary key,
    defining required properties for `Amount`, `Date`, and
    `PaymentMethod`, and establishing a default value for
    `Date` using SQL's `GETDATE()` function.

---
+++ Cinema.DAL/Configurations/PaymentConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Cinema.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cinema.DAL.Configurations;
+
+public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+{
+    public void Configure(EntityTypeBuilder<Payment> builder)
+    {
+        builder
+            .HasKey(p => p.Id);
+
+        builder
+            .Property(p => p.Amount)
+            .IsRequired();
+
+        builder
+            .Property(p => p.Date)
+            .HasDefaultValueSql("GETDATE()")
+            .IsRequired();
+
+        builder
+            .Property(p => p.PaymentMethod)
+            .HasMaxLength(60)
+            .IsRequired();
+    }
+}
