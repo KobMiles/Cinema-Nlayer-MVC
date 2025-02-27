d68fabd3a869b6c3d6b6a884d8963b0b744ab32d
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-28 01:08:33 +0200

    Add TicketConfiguration for entity setup
    Introduces the `TicketConfiguration` class in the `Cinema.DAL.Configurations` namespace. This class configures the `Ticket` entity using `EntityTypeBuilder<Ticket>`, defining the primary key and establishing relationships with `Session`, `Payment`, `Seat`, and `User`, including foreign key constraints and cascade delete behavior where applicable.

---
+++ Cinema.DAL/Configurations/TicketConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Cinema.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cinema.DAL.Configurations;
+
+public class TicketConfiguration
+{
+    public void Configure(EntityTypeBuilder<Ticket> builder)
+    {
+        builder
+            .HasKey(t => t.Id);
+
+        builder
+            .HasOne(t => t.Session)
+            .WithMany(s => s.Tickets)
+            .HasForeignKey(t => t.SessionId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder
+            .HasOne(t => t.Payment)
+            .WithMany(p => p.Tickets)
+            .HasForeignKey(t => t.PaymentId);
+
+        builder
+            .HasOne(t => t.Seat)
+            .WithMany(p => p.Tickets)
+            .HasForeignKey(t => t.SeatId);
+
+        builder
+            .HasOne(t => t.User)
+            .WithMany(u => u.Tickets)
+            .HasForeignKey(t => t.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
