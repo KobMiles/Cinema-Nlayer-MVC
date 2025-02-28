d2ce76a784117d3767a018a235ceeeafa68a162a
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-28 11:54:02 +0200

    Add UserClaimConfiguration for EF Core mapping
    Introduces a new class `UserClaimConfiguration` that implements
    the `IEntityTypeConfiguration<IdentityUserClaim<string>>`
    interface to configure the `UserClaims` table in the database.
    Necessary namespaces for `IdentityUserClaim` and
    `EntityTypeBuilder` are also imported.

---
+++ Cinema.DAL/Configurations/Identity/UserClaimConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.DAL.Configurations.Identity;
+public class UserClaimConfiguration : IEntityTypeConfiguration<IdentityUserClaim<string>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserClaim<string>> builder)
+    {
+        builder.ToTable("UserClaims");
+    }
+}
