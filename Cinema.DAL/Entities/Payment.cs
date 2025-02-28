8c5050e96c9f5ec3dba9aedf9d564f2799e4b1a6
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-28 12:30:08 +0200

    Add payment method and status enums to Payment class
    Introduce `PaymentMethod` and `PaymentStatus` enums to define payment options and statuses. Update the `Payment` class to replace the `Date` and `string PaymentMethod` properties with `PaymentDate` and the new enum types. Initialize the `Tickets` property with an empty array.

--- Cinema.DAL/Entities/Payment.cs
+++ Cinema.DAL/Entities/Payment.cs
@@ -1,12 +1,26 @@
 ﻿using Cinema.DAL.Interfaces;
 
 namespace Cinema.DAL.Entities;
+public enum PaymentMethod
+{
+    Card,
+    Cash
+}
+
+public enum PaymentStatus
+{
+    Pending, 
+    Completed,
+    Cancelled
+}
+
 public class Payment : IEntity
 {
     public int Id { get; set; }
     public decimal Amount { get; set; }
-    public DateTime Date { get; set; }
-    public string PaymentMethod { get; set; } = null!;
+    public DateTime PaymentDate { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+    public PaymentStatus PaymentStatus { get; set; }
 
     public ICollection<Ticket> Tickets { get; set; } = [];
 }
