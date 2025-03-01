9755527778a1008b324b3733385009169ee2fd5e
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-01 19:01:00 +0200

    Add IRepository interface for generic CRUD operations
    Introduces the `IRepository<TEntity>` interface in the
    `Cinema.DAL.Interfaces.Repositories` namespace. This
    generic interface, constrained to `IEntity` types,
    provides methods for basic CRUD operations:
    `GetAll`, `GetById`, `Insert`, `Update`, `Delete`,
    and a `Find` method for querying entities with a
    predicate. This implementation enhances the repository
    pattern for better data interaction.

---
+++ Cinema.DAL/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.Specification;
+using System.Linq.Expressions;
+
+namespace Cinema.DAL.Interfaces.Repositories;
+
+public interface IRepository<TEntity> where TEntity : class, IEntity
+{
+    IQueryable<TEntity> GetAll();
+    TEntity? GetById(int id);
+    void Insert(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(int id);
+    void Delete(TEntity entity);
+
+    IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+}
