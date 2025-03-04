ab3c4113fdbba0f2d4b058337523cb2038ece483
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-05 00:45:16 +0200

    Refactor IRepository to inherit from base interfaces
    Updated the `IRepository<TEntity>` interface to inherit from `IRepositoryBase<TEntity>` and `IReadRepository<TEntity>`, enhancing its functionality. Removed all method signatures, allowing reliance on inherited methods from the base interfaces.

--- Cinema.DAL/Interfaces/Repositories/IRepository.cs
+++ Cinema.DAL/Interfaces/Repositories/IRepository.cs
@@ -3,14 +3,7 @@ using System.Linq.Expressions;
 
 namespace Cinema.DAL.Interfaces.Repositories;
 
-public interface IRepository<TEntity> where TEntity : class, IEntity
+public interface IRepository<TEntity> : IRepositoryBase<TEntity>, IReadRepository<TEntity>
+    where TEntity : class, IEntity
 {
-    IQueryable<TEntity> GetAll();
-    TEntity? GetById(int id);
-    void Insert(TEntity entity);
-    void Update(TEntity entity);
-    void Delete(int id);
-    void Delete(TEntity entity);
-
-    IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
 }
