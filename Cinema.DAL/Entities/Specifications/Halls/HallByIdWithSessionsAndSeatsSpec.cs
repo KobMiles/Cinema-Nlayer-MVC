6ba0a73d88adc2119b4c8f321f21a64eb8eb8878
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-05 01:14:48 +0200

    Add HallByIdWithSessionsAndSeatsSpec specification
    Introduced a new specification class `HallByIdWithSessionsAndSeatsSpec` in the `Cinema.DAL.Entities.Specifications.Halls` namespace. This class allows querying a `Hall` entity by its ID while including related `Sessions` and `Seats` in the results.

---
+++ Cinema.DAL/Entities/Specifications/Halls/HallByIdWithSessionsAndSeatsSpec.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+
+namespace Cinema.DAL.Entities.Specifications.Halls;
+
+public class HallByIdWithSessionsAndSeatsSpec : Specification<Hall>
+{
+    public HallByIdWithSessionsAndSeatsSpec(int hallId)
+    {
+        Query.Where(h => h.Id == hallId)
+            .Include(h => h.Sessions)
+            .Include(h => h.Seats);
+    }
+}
