aeea1ad72543f5e749d2ce80a8984194897bc0b5
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-06 13:57:09 +0200

    Update Hall.cs namespace and collection initializations
    Changed the namespace to `Cinema.DAL.Entities` and updated
    the initialization of `Sessions` and `Seats` properties
    to use `new List<Session>()` and `new List<Seat>()`
    for proper collection instantiation.

--- Cinema.DAL/Entities/Hall.cs
+++ Cinema.DAL/Entities/Hall.cs
@@ -7,6 +7,5 @@ public class Hall : IEntity
     public int Id { get; set; }
 
     public ICollection<Session> Sessions { get; set; } = [];
-
     public ICollection<Seat> Seats { get; set; } = [];
 }
