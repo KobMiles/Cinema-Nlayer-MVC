8a3e50834d584345286ab5f3339b06a60e0b0282
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-06 13:44:58 +0200

    Add UserConfiguration for Entity Framework mapping
    Introduced a new class `UserConfiguration` that implements
    the `IEntityTypeConfiguration<User>` interface. This class
    configures the `User` entity to map to the "Users" table
    in the database for Entity Framework Core.

--- Cinema.DAL/Configurations/Identity/UserConfiguration.cs
+++ Cinema.DAL/Configurations/Identity/UserConfiguration.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Cinema.DAL.Configurations.Identity;
+
 public class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
