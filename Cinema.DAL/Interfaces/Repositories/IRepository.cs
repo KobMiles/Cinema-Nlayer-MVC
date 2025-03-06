9748acabc3bd4446c93e0253930767730fa88a9a
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-06 14:04:55 +0200

    Remove unused namespace from IRepository.cs
    The line `using System.Linq.Expressions;` was removed from the `IRepository.cs` file, indicating that the code no longer requires this namespace. This change may reflect a cleanup of unused code or a shift in functionality.

--- Cinema.DAL/Interfaces/Repositories/IRepository.cs
+++ Cinema.DAL/Interfaces/Repositories/IRepository.cs
@@ -1,9 +1,9 @@
 ﻿using Ardalis.Specification;
-using System.Linq.Expressions;
 
 namespace Cinema.DAL.Interfaces.Repositories;
 
 public interface IRepository<TEntity> : IRepositoryBase<TEntity>, IReadRepository<TEntity>
     where TEntity : class, IEntity
 {
+
 }
