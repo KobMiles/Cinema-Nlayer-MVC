8ac960306ce654cca4608bf06fff580228a73ac9
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-10 17:14:12 +0200

    Add SeatDto class for seat management
    Introduced a new `SeatDto` class in the `Cinema.BLL.DTOs.Seats` namespace.
    This class contains four integer properties: `Id`, `SeatRow`, `SeatNumber`,
    and `HallId`, each with public getters and setters to facilitate seat management.

---
+++ Cinema.BLL/DTOs/Seats/SeatDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinema.BLL.DTOs.Seats;
+
+public class SeatDto
+{
+    public int Id { get; set; }
+    public int SeatRow { get; set; }
+    public int SeatNumber { get; set; }
+    public int HallId { get; set; }
+}
