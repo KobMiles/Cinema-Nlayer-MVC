6544239fe51a9833bae476caf1b93ed17da6693e
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-10 17:14:06 +0200

    Add PaymentDto class for payment processing
    Introduced the `PaymentDto` class in the `Cinema.BLL.DTOs.Payments` namespace. This class includes properties for `Id`, `Amount`, `PaymentDate`, `PaymentMethod`, and `PaymentStatus`. Also added an import statement for `Cinema.DAL.Entities.Enums` to support the new properties.

---
+++ Cinema.BLL/DTOs/Payments/PaymentDto.cs
@@ -0,0 +1,12 @@
+﻿using Cinema.DAL.Entities.Enums;
+
+namespace Cinema.BLL.DTOs.Payments;
+
+public class PaymentDto
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime PaymentDate { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+    public PaymentStatus PaymentStatus { get; set; }
+}
