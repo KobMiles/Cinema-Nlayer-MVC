75423164c294b46b84f0f6c9ac61225967822b8f
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-12 21:12:59 +0200

    Add SessionDetailsWithTicketsDto class
    Introduces a new DTO class for session details, including properties for session ID, movie and hall information, start time, ticket price, and a collection of tickets. Imports necessary DTOs for Halls, Movies, and Tickets.

---
+++ Cinema.BLL/DTOs/Sessions/SessionDetailsWithTicketsDto.cs
@@ -0,0 +1,21 @@
+﻿using Cinema.BLL.DTOs.Halls;
+using Cinema.BLL.DTOs.Movies;
+using Cinema.BLL.DTOs.Tickets;
+
+namespace Cinema.BLL.DTOs.Sessions;
+
+public class SessionDetailsWithTicketsDto
+{
+    public int Id { get; set; }
+
+    public int MovieId { get; set; }
+    public MovieDto Movie { get; set; } = null!;
+
+    public int HallId { get; set; }
+    public HallDto Hall { get; set; } = null!;
+
+    public DateTime StartTime { get; set; }
+    public decimal TicketPrice { get; set; }
+
+    public IReadOnlyCollection<TicketDto> Tickets { get; set; } = [];
+}
