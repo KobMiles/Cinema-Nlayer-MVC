62b6c21db227c4c74299d6c2f07a2dd61c0f7bbe
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-12 21:12:28 +0200

    Add MovieDetailsDto class for movie information
    Introduces the MovieDetailsDto class in the Cinema.BLL.DTOs.Movies namespace.
    This class includes properties for Id, Name, Description, RatingScore, Duration,
    ReleaseDate, PosterUrl, and TrailerUrl. It also contains IReadOnlyCollection
    properties for Genres and Sessions, initialized as empty collections.
    Necessary using directives for GenreDto and SessionDto are added.

---
+++ Cinema.BLL/DTOs/Movies/MovieDetailsDto.cs
@@ -0,0 +1,19 @@
+﻿using Cinema.BLL.DTOs.Genres;
+using Cinema.BLL.DTOs.Sessions;
+
+namespace Cinema.BLL.DTOs.Movies;
+
+public class MovieDetailsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public float RatingScore { get; set; }
+    public TimeSpan Duration { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public string PosterUrl { get; set; } = null!;
+    public string TrailerUrl { get; set; } = null!;
+
+    public IReadOnlyCollection<GenreDto> Genres { get; set; } = [];
+    public IReadOnlyCollection<SessionDto> Sessions { get; set; } = [];
+}
