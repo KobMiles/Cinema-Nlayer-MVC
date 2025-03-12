74da466019b5020855db70681717f2174ed09e21
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-12 21:12:52 +0200

    Add SessionDetailsWithoutTicketsDto class
    Introduces the `SessionDetailsWithoutTicketsDto` class in the `Cinema.BLL.DTOs.Sessions` namespace. This class includes properties for session details such as `Id`, `MovieId`, `Movie`, `HallId`, `Hall`, `StartTime`, and `TicketPrice`. Also adds necessary using directives for `HallDto` and `MovieDto`.

---
+++ Cinema.BLL/DTOs/Sessions/SessionDetailsWithoutTicketsDto.cs
@@ -0,0 +1,18 @@
+﻿using Cinema.BLL.DTOs.Halls;
+using Cinema.BLL.DTOs.Movies;
+
+namespace Cinema.BLL.DTOs.Sessions;
+
+public class SessionDetailsWithoutTicketsDto
+{
+    public int Id { get; set; }
+
+    public int MovieId { get; set; }
+    public MovieDto Movie { get; set; } = null!;
+
+    public int HallId { get; set; }
+    public HallDto Hall { get; set; } = null!;
+
+    public DateTime StartTime { get; set; }
+    public decimal TicketPrice { get; set; }
+}
