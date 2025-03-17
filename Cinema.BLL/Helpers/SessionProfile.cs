71ec1b9b1f1a0bef73dc282ed83edf74d3344b67
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-17 15:52:48 +0200

    Add SessionProfile for AutoMapper configurations
    Introduces a new `SessionProfile` class in the `Cinema.BLL.Helpers` namespace. This class sets up AutoMapper mappings between the `Session` entity and the corresponding Data Transfer Objects: `SessionDto`, `SessionDetailsWithoutTicketsDto`, and `SessionDetailsWithTicketsDto`. Necessary using directives for AutoMapper and the DTOs are also included.

---
+++ Cinema.BLL/Helpers/SessionProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Cinema.BLL.DTOs.Sessions;
+using Cinema.DAL.Entities;
+
+namespace Cinema.BLL.Helpers;
+
+public class SessionProfile : Profile
+{
+    public SessionProfile()
+    {
+        CreateMap<Session, SessionDto>();
+
+        CreateMap<Session, SessionDetailsWithoutTicketsDto>();
+
+        CreateMap<Session, SessionDetailsWithTicketsDto>();
+    }
+}
