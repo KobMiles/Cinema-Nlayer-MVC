04ea7974041ae9406f21ed2d948add3c4b6f6f76
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-17 15:52:55 +0200

    Add TicketProfile for AutoMapper configurations
    Introduced a new `TicketProfile` class in the `Cinema.BLL.Helpers` namespace. This class sets up AutoMapper mappings between the `Ticket` entity and the `TicketDto` and `TicketDetailsDto` data transfer objects (DTOs) within its constructor.

---
+++ Cinema.BLL/Helpers/TicketProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Cinema.BLL.DTOs.Tickets;
+using Cinema.DAL.Entities;
+
+namespace Cinema.BLL.Helpers;
+
+public class TicketProfile : Profile
+{
+    public TicketProfile()
+    {
+        CreateMap<Ticket, TicketDto>();
+
+        CreateMap<Ticket, TicketDetailsDto>();
+    }
+}
