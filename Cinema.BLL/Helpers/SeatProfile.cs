b6d5ad110bc134cad1b98f2ec903093918b662fc
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-17 15:52:37 +0200

    Add SeatProfile for AutoMapper configurations
    Introduces the `SeatProfile` class in the `Cinema.BLL.Helpers` namespace to set up AutoMapper mappings between the `Seat` entity and `SeatDto`/`SeatDetailsDto`. This enables seamless data transformation between entity and DTO representations.

---
+++ Cinema.BLL/Helpers/SeatProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Cinema.BLL.DTOs.Seats;
+using Cinema.DAL.Entities;
+
+namespace Cinema.BLL.Helpers;
+
+public class SeatProfile : Profile
+{
+    public SeatProfile()
+    {
+        CreateMap<Seat, SeatDto>();
+
+        CreateMap<Seat, SeatDetailsDto>();
+    }
+}
