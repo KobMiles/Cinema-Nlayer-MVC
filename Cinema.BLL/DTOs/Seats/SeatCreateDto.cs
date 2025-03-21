7b728882ed1746832b84a0c43e6d6838697d7b19
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-21 10:51:25 +0200

    Add SeatCreateDto class for seat creation
    Introduced a new `SeatCreateDto` class in the `Cinema.BLL.DTOs.Seats` namespace. This class contains three integer properties: `SeatRow`, `SeatNumber`, and `HallId`, with public getters and setters to facilitate seat creation functionality.

---
+++ Cinema.BLL/DTOs/Seats/SeatCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Cinema.BLL.DTOs.Seats;
+
+public class SeatCreateDto
+{
+    public int SeatRow { get; set; }
+    public int SeatNumber { get; set; }
+    public int HallId { get; set; }
+}
