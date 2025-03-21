bf96c04ee82748126761b5d4d4b92f823d43ea93
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-22 01:13:32 +0200

    Add UserRegisterDto class for user registration
    Introduced a new `UserRegisterDto` class in the
    `Cinema.BLL.DTOs.Users` namespace. This class contains
    four properties: `Email`, `Password`, `ConfirmPassword`,
    and `PhoneNumber`, all of which are required and
    initialized to `null!`.

---
+++ Cinema.BLL/DTOs/Users/UserRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinema.BLL.DTOs.Users;
+
+public class UserRegisterDto
+{
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string ConfirmPassword { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+}
