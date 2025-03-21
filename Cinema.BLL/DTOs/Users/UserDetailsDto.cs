a4ab5b318ddb8118d88ea544dfe04654b444064e
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-22 01:13:09 +0200

    Remove confirmation properties from UserDetailsDto
    The `UserDetailsDto` class has been updated to remove the
    `EmailConfirmed` and `PhoneNumberConfirmed` properties.
    These changes streamline the class by eliminating unnecessary
    confirmation flags while retaining the existing properties
    such as `Id`, `UserName`, `Email`, `PhoneNumber`, and
    `Tickets`.

--- Cinema.BLL/DTOs/Users/UserDetailsDto.cs
+++ Cinema.BLL/DTOs/Users/UserDetailsDto.cs
@@ -7,9 +7,7 @@ public class UserDetailsDto
     public string Id { get; set; } = null!;
     public string UserName { get; set; } = null!;
     public string Email { get; set; } = null!;
-    public bool EmailConfirmed { get; set; }
     public string? PhoneNumber { get; set; }
-    public bool PhoneNumberConfirmed { get; set; }
 
     public IReadOnlyCollection<TicketDto> Tickets { get; set; } = [];
 }
