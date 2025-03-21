6e847006a9ea8c3a9904cd11f73614b4413c43dc
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-21 18:32:26 +0200

    Add MoviesWithActiveSessionsSpecification class
    Introduces a new specification class to filter movies
    with active sessions. The class queries for movies that
    have sessions starting from the current time and includes
    the related sessions in the results.

---
+++ Cinema.DAL/Entities/Specifications/Movies/MoviesWithActiveSessionsSpecification.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+
+namespace Cinema.DAL.Entities.Specifications.Movies;
+
+public class MoviesWithActiveSessionsSpecification : Specification<Movie>
+{
+    public MoviesWithActiveSessionsSpecification()
+    {
+        Query
+            .Where(movie => movie.Sessions.Any(s => s.StartTime >= DateTime.Now))
+            .Include(movie => movie.Sessions);
+    }
+}
