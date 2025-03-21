a52b7b5356306237973d78dc309f880a2842ef2c
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-21 10:51:10 +0200

    Add PaymentCreateDto for payment processing
    Introduces the `PaymentCreateDto` class in the
    `Cinema.BLL.DTOs.Payments` namespace. This class
    includes properties for `Amount` (decimal) and
    `PaymentMethod` (enum), facilitating the creation
    of payment-related data transfer objects.

---
+++ Cinema.BLL/DTOs/Payments/PaymentCreateDto.cs
@@ -0,0 +1,9 @@
+﻿using Cinema.DAL.Entities.Enums;
+
+namespace Cinema.BLL.DTOs.Payments;
+
+public class PaymentCreateDto
+{
+    public decimal Amount { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+}
