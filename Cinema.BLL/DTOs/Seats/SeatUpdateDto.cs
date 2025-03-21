9e0568a4bfad9648c2c670c8a9301f700b7841f8
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-21 10:51:31 +0200

    Add SeatUpdateDto class for seat updates
    Introduced a new class `SeatUpdateDto` in the `Cinema.BLL.DTOs.Seats` namespace. This class contains four integer properties: `Id`, `SeatRow`, `SeatNumber`, and `HallId`, each with public getters and setters to facilitate seat update operations.

---
+++ Cinema.BLL/DTOs/Seats/SeatUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinema.BLL.DTOs.Seats;
+
+public class SeatUpdateDto
+{
+    public int Id { get; set; }
+    public int SeatRow { get; set; }
+    public int SeatNumber { get; set; }
+    public int HallId { get; set; }
+}
