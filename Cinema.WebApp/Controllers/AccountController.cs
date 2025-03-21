2642fba93ead5ab13a272a59f6a46b4980804dd6
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-22 01:27:43 +0200

    Add AccountController for user management features
    Implemented user registration, login, logout, and profile management in the new AccountController. Utilized dependency injection for authentication and user services. Added error handling for login attempts and profile retrieval, with appropriate HTTP responses.

---
+++ Cinema.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using Cinema.BLL.DTOs.Users;
+using Cinema.BLL.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Cinema.WebApp.Controllers
+{
+    public class AccountController(IAuthService authService, IUserService userService) : Controller
+    {
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto model)
+        {
+            var result = await authService.RegisterAsync(model);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginDto model, string? returnUrl = null)
+        {
+            var result = await authService.LoginAsync(model);
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await authService.LogoutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized();
+            }
+
+            UserDetailsDto userDetails;
+            try
+            {
+                userDetails = await userService.GetUserDetailsAsync(userId);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+
+            return View(userDetails);
+        }
+    }
+}
