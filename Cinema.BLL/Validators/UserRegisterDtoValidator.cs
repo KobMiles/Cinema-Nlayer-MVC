4bff34f6d4b57052e6ac9385e3023cd5dee6a4bf
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-22 01:18:23 +0200

    Add UserRegisterDtoValidator for registration validation
    Introduces a new `UserRegisterDtoValidator` class that
    validates user registration data, including rules for
    email, password, confirm password, and phone number
    fields to ensure they meet specified criteria.

---
+++ Cinema.BLL/Validators/UserRegisterDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using Cinema.BLL.DTOs.Users;
+using FluentValidation;
+
+namespace Cinema.BLL.Validators;
+
+public class UserRegisterDtoValidator : AbstractValidator<UserRegisterDto>
+{
+    public UserRegisterDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email address.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.Password).WithMessage("Passwords do not match.");
+
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("Phone number is required.");
+    }
+}
