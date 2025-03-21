31d915c26bba023bfdbcb67353146b597fb497b3
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-22 01:17:43 +0200

    Add AuthService for user management functionality
    Introduces the `AuthService` class in the `Cinema.BLL.Services` namespace, implementing the `IAuthService` interface. This class provides methods for user registration, login, and logout, utilizing AutoMapper for DTO mapping and ASP.NET Core Identity for user management.

---
+++ Cinema.BLL/Services/AuthService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Cinema.BLL.DTOs.Users;
+using Cinema.BLL.Interfaces.Services;
+using Cinema.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Cinema.BLL.Services;
+
+public class AuthService(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper) : IAuthService
+{
+    public async Task<IdentityResult> RegisterAsync(UserRegisterDto registerDto)
+    {
+        var user = mapper.Map<User>(registerDto);
+
+        var result = await userManager.CreateAsync(user, registerDto.Password);
+
+        return result;
+    }
+
+    public async Task<SignInResult> LoginAsync(UserLoginDto loginDto)
+    {
+        var user = await userManager.FindByEmailAsync(loginDto.Email);
+        if (user == null)
+        {
+            return SignInResult.Failed;
+        }
+
+        var result = await signInManager.PasswordSignInAsync(user, loginDto.Password, loginDto.RememberMe, lockoutOnFailure: true);
+        return result;
+    }
+
+    public async Task LogoutAsync()
+    {
+        await signInManager.SignOutAsync();
+    }
+}
