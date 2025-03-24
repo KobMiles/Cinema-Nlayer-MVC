613cf107cbd829b506199a27264e55cb90acaa83
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-24 21:49:14 +0200

    Refactor AuthService for improved readability and logic
    - Updated constructor for clearer parameter definition.
    - Changed LoginAsync return type to include redirect URL.
    - Enhanced LoginAsync logic for handling sign-in results.
    - Simplified RegisterAsync method for better clarity.

--- Cinema.BLL/Services/AuthService.cs
+++ Cinema.BLL/Services/AuthService.cs
@@ -3,30 +3,42 @@ using Cinema.BLL.DTOs.Users;
 using Cinema.BLL.Interfaces.Services;
 using Cinema.DAL.Entities;
 using Microsoft.AspNetCore.Identity;
+using IdentitySignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cinema.BLL.Services;
 
-public class AuthService(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper) : IAuthService
+public class AuthService(
+    UserManager<User> userManager,
+    SignInManager<User> signInManager,
+    IMapper mapper) : IAuthService
 {
     public async Task<IdentityResult> RegisterAsync(UserRegisterDto registerDto)
     {
         var user = mapper.Map<User>(registerDto);
-
-        var result = await userManager.CreateAsync(user, registerDto.Password);
-
-        return result;
+        return await userManager.CreateAsync(user, registerDto.Password);
     }
 
-    public async Task<SignInResult> LoginAsync(UserLoginDto loginDto)
+    public async Task<(IdentitySignInResult result, string redirectUrl)> LoginAsync(UserLoginDto loginDto, string? returnUrl, IUrlHelper urlHelper)
     {
         var user = await userManager.FindByEmailAsync(loginDto.Email);
         if (user == null)
         {
-            return SignInResult.Failed;
+            return (IdentitySignInResult.Failed, string.Empty);
         }
 
         var result = await signInManager.PasswordSignInAsync(user, loginDto.Password, loginDto.RememberMe, lockoutOnFailure: true);
-        return result;
+
+        if (result.Succeeded)
+        {
+            var safeRedirectUrl = urlHelper.IsLocalUrl(returnUrl)
+                ? returnUrl
+                : urlHelper.Action("Index", "Home")!;
+
+            return (result, safeRedirectUrl);
+        }
+
+        return (result, string.Empty);
     }
 
     public async Task LogoutAsync()
