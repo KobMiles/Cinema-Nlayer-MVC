f8466fc860a33b3e256782231e367fdabaf68477
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-26 12:04:35 +0200

    Refactor IAuthService for login and logout methods
    Removed the old LoginAsync method that returned a tuple
    and replaced it with a simplified version returning only
    IdentitySignInResult. Added a new LogoutAsync method
    to handle user logout functionality.

--- Cinema.BLL/Interfaces/Services/IAuthService.cs
+++ Cinema.BLL/Interfaces/Services/IAuthService.cs
@@ -1,13 +1,14 @@
 ﻿using Cinema.BLL.DTOs.Users;
 using Microsoft.AspNetCore.Identity;
 using IdentitySignInResult = Microsoft.AspNetCore.Identity.SignInResult;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Cinema.BLL.Interfaces.Services;
 
 public interface IAuthService
 {
     Task<IdentityResult> RegisterAsync(UserRegisterDto registerDto);
-    Task<(IdentitySignInResult result, string redirectUrl)> LoginAsync(UserLoginDto loginDto, string? returnUrl, IUrlHelper urlHelper);
+
+    Task<IdentitySignInResult> LoginAsync(UserLoginDto loginDto);
+
     Task LogoutAsync();
 }
