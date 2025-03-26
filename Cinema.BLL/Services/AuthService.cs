1102b9e4b83a4624c35c2a75185cddcafd9820e5
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-26 12:04:42 +0200

    Refactor AuthService LoginAsync method
    Simplified the LoginAsync method by removing the returnUrl and urlHelper parameters. The method now returns only an IdentitySignInResult instead of a tuple, eliminating local URL redirection logic. Additionally, changed the lockoutOnFailure parameter in PasswordSignInAsync from true to false, streamlining the login process.

--- Cinema.BLL/Services/AuthService.cs
+++ Cinema.BLL/Services/AuthService.cs
@@ -4,7 +4,6 @@ using Cinema.BLL.Interfaces.Services;
 using Cinema.DAL.Entities;
 using Microsoft.AspNetCore.Identity;
 using IdentitySignInResult = Microsoft.AspNetCore.Identity.SignInResult;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Cinema.BLL.Services;
 
@@ -19,26 +18,22 @@ public class AuthService(
         return await userManager.CreateAsync(user, registerDto.Password);
     }
 
-    public async Task<(IdentitySignInResult result, string redirectUrl)> LoginAsync(UserLoginDto loginDto, string? returnUrl, IUrlHelper urlHelper)
+    public async Task<IdentitySignInResult> LoginAsync(UserLoginDto loginDto)
     {
         var user = await userManager.FindByEmailAsync(loginDto.Email);
         if (user == null)
         {
-            return (IdentitySignInResult.Failed, string.Empty);
+            return IdentitySignInResult.Failed;
         }
 
-        var result = await signInManager.PasswordSignInAsync(user, loginDto.Password, loginDto.RememberMe, lockoutOnFailure: true);
+        var result = await signInManager.PasswordSignInAsync(
+            user,
+            loginDto.Password,
+            loginDto.RememberMe,
+            lockoutOnFailure: false
+        );
 
-        if (result.Succeeded)
-        {
-            var safeRedirectUrl = urlHelper.IsLocalUrl(returnUrl)
-                ? returnUrl
-                : urlHelper.Action("Index", "Home")!;
-
-            return (result, safeRedirectUrl);
-        }
-
-        return (result, string.Empty);
+        return result;
     }
 
     public async Task LogoutAsync()
