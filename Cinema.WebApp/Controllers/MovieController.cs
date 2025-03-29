91050295923ee96812e84d751814e713b2af53bf
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-03-29 21:18:07 +0200

    Add MovieController for movie detail retrieval
    Introduces a new `MovieController` class in the `Cinema.WebApp.Controllers` namespace. The controller includes an `Index` method that asynchronously fetches movie details using an `IMovieService` instance. It handles cases where the movie is not found by returning a `NotFound` result, or it returns a view with the movie details if found.

---
+++ Cinema.WebApp/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using Cinema.BLL.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema.WebApp.Controllers;
+
+public class MovieController(IMovieService movieService) : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Index(int id)
+    {
+        var movieDetails = await movieService.GetMovieDetailsAsync(id);
+
+        if (movieDetails == null)
+            return NotFound();
+
+        return View(movieDetails);
+    }
+}
