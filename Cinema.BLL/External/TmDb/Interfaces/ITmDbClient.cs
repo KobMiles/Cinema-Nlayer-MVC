1c9871c334f120bdf9ba34279ec930a379909a32
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-30 17:57:35 +0300

    Add ITmDbClient interface for movie search and details
    Introduces the `ITmDbClient` interface in the
    `Cinema.BLL.External.TmDb.Interfaces` namespace.
    This interface defines two asynchronous methods:
    `SearchMovieAsync` for searching movies and
    `GetMovieDetailsAsync` for retrieving movie details.
    A using directive for `Cinema.BLL.External.TmDb.DTOs`
    is also added to support the DTOs used in the interface.

---
+++ Cinema.BLL/External/TmDb/Interfaces/ITmDbClient.cs
@@ -0,0 +1,10 @@
+﻿using Cinema.BLL.External.TmDb.DTOs;
+
+namespace Cinema.BLL.External.TmDb.Interfaces;
+
+public interface ITmDbClient
+{
+    Task<IReadOnlyCollection<TmDbMovieSearchItemDto>> SearchMovieAsync(string query);
+
+    Task<TmDbMovieDetailsDto?> GetMovieDetailsAsync(int tmDbId);
+}
