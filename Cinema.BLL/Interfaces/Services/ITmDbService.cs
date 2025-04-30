f12896b490a66edbce298d5e2d5968d42667d736
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-30 17:59:02 +0300

    Add ITmDbService interface for movie operations
    This commit introduces the `ITmDbService` interface in the
    `Cinema.BLL.Interfaces.Services` namespace. It defines two
    asynchronous methods: `SearchMoviesAsync` for retrieving
    a list of `MovieDto` objects based on a search query,
    and `GetMovieForCreateAsync` for fetching a `MovieCreateDto`
    object using a specified `tmDbId`. The necessary DTOs are
    imported from the `Cinema.BLL.DTOs.Movies` namespace.

---
+++ Cinema.BLL/Interfaces/Services/ITmDbService.cs
@@ -0,0 +1,9 @@
+﻿using Cinema.BLL.DTOs.Movies;
+
+namespace Cinema.BLL.Interfaces.Services;
+
+public interface ITmDbService
+{
+    Task<IEnumerable<MovieDto>> SearchMoviesAsync(string query);
+    Task<MovieCreateDto?> GetMovieForCreateAsync(int tmDbId);
+}
