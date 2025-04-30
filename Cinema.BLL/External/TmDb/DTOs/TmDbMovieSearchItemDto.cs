cae87330b08586c833f6e1928231a53dc4fd20a7
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-30 17:57:19 +0300

    Add DTOs for TMDb API movie data handling
    Introduced new data transfer objects (DTOs) including
    TmDbGenreDto, TmDbMovieDetailsDto, TmDbMovieSearchItemDto,
    TmDbSearchMovieResponseDto, TmDbTrailerDto, and
    TmDbTrailersResponseDto. Each DTO is equipped with
    JsonPropertyName attributes for JSON serialization,
    facilitating accurate data mapping with the TMDb API.
    Nullable types are utilized for optional fields.

---
+++ Cinema.BLL/External/TmDb/DTOs/TmDbMovieSearchItemDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Cinema.BLL.External.TmDb.DTOs;
+
+public class TmDbMovieSearchItemDto
+{
+    [JsonPropertyName("id")]
+    public int Id { get; init; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; init; } = null!;
+
+    [JsonPropertyName("poster_path")]
+    public string? PosterPath { get; init; }
+
+    [JsonIgnore]
+    public string PosterUrl { get; set; } = string.Empty;
+}
---
+++ Cinema.BLL/External/TmDb/DTOs/TmDbTrailerDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Cinema.BLL.External.TmDb.DTOs;
+
+public class TmDbTrailerDto
+{
+    [JsonPropertyName("key")]
+    public string Key { get; init; } = null!;
+
+    [JsonPropertyName("site")]
+    public string Site { get; init; } = null!;
+
+    [JsonPropertyName("type")]
+    public string Type { get; init; } = null!;
+}
