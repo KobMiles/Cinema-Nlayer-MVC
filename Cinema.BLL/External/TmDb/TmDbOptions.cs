883c58e6eafed13e42dd9b211ff382323e89958d
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-30 17:57:59 +0300

    Add TmDbOptions class for TMDb API configuration
    Introduced a new `TmDbOptions` class in the `Cinema.BLL.External.TmDb` namespace. This class contains properties for TMDb API configuration, including required `ApiKey` and default values for `BaseUrl`, `ImageBaseUrl`, `Language`, `PosterSize`, and `YoutubeBase`.

---
+++ Cinema.BLL/External/TmDb/TmDbOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Cinema.BLL.External.TmDb;
+
+public class TmDbOptions
+{
+    public const string SectionName = "Tmdb";
+
+    public required string ApiKey { get; init; }
+
+    public string BaseUrl { get; init; } = "https://api.themoviedb.org/3/";
+    public string ImageBaseUrl { get; init; } = "https://image.tmdb.org/t/p/";
+    public string Language { get; init; } = "en-US";
+    public string PosterSize { get; init; } = "w500";
+    public string YoutubeBase { get; init; } = "https://www.youtube.com/embed/";
+}
