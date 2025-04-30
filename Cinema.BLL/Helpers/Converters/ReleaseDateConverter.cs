b49662f36ed6b76fa9808e3f3fd72d16b0445a14
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-04-30 17:58:12 +0300

    Add ReleaseDateConverter and RuntimeConverter classes
    Introduce two new converter classes in the Cinema.BLL.Helpers.Converters namespace.
    `ReleaseDateConverter` converts string dates to nullable DateTime,
    and `RuntimeConverter` converts integer minutes to TimeSpan.
    Both implement the IValueConverter interface from AutoMapper.

---
+++ Cinema.BLL/Helpers/Converters/ReleaseDateConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using AutoMapper;
+
+namespace Cinema.BLL.Helpers.Converters;
+
+
+public class ReleaseDateConverter : IValueConverter<string?, DateTime?>
+{
+    private static readonly string[] Formats = ["yyyy-MM-dd"];
+    public DateTime? Convert(string? source, ResolutionContext _)
+    {
+        if (string.IsNullOrWhiteSpace(source)) return null;
+        return DateTime.TryParseExact(source, Formats, CultureInfo.InvariantCulture,
+            DateTimeStyles.None, out var dt)
+            ? dt : null;
+    }
+}
