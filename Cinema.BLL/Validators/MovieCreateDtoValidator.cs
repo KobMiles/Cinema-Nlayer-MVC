59dfa2c41876647aedfea55b6e8e5191b12ef120
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-05-01 19:00:51 +0300

    Increase Description length in MovieCreateDtoValidator
    Updated the maximum length for the `Description` field
    from 600 characters to 1000 characters in the
    `MovieCreateDtoValidator` class to allow for more
    detailed descriptions.

--- Cinema.BLL/Validators/MovieCreateDtoValidator.cs
+++ Cinema.BLL/Validators/MovieCreateDtoValidator.cs
@@ -8,7 +8,7 @@ public sealed class MovieCreateDtoValidator : AbstractValidator<MovieCreateDto>
     public MovieCreateDtoValidator()
     {
         RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
-        RuleFor(x => x.Description).MaximumLength(600);
+        RuleFor(x => x.Description).MaximumLength(1000);
         RuleFor(x => x.RatingScore).InclusiveBetween(0f, 10f);
         RuleFor(x => x.Duration).GreaterThan(TimeSpan.Zero);
         RuleFor(x => x.PosterUrl).NotEmpty().MaximumLength(300);
